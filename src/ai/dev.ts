8400440ece3575e04e86c3e7bafc8a15f5aba020
MR PQR <pqrmr0162@gmail.com>
2025-08-26 14:18:06 +0000

    this issue is showing

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,6 +1,3 @@
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/dynamic-tool-selection.ts';
 import '@/ai/flows/interpret-prompt.ts';
 import '@/ai/flows/generate-image.ts';
--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import { config } from 'dotenv';
+
+config({ path: '.env' });
 
 export const ai = genkit({
   plugins: [googleAI()],
